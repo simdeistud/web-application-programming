f4428c3e90f8b3fb47f54c745d373241fd96dbb6
simdeistud <simone.deiana@studenti.units.it>
2025-11-23 11:13:14 +0100

    Added initial structure for auth management

--- EXAM CALCETTO PROJECT/src/server/api.js
+++ EXAM CALCETTO PROJECT/src/server/api.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const auth = require("./auth.js");
 const router = express.Router();
+
+app.use("/auth", auth);
+	
+router.get("/whoami", (req, res, next) => { 
+    res.writeHead(401);
+    res.end("ERROR: LOGIN FIRST TO SEE YOUR INFORMATION");
+  });
  
 module.exports = router;
---
+++ EXAM CALCETTO PROJECT/src/server/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+    
+router.post("/signup", (req, res) => {
+    /* handle signup */
+  });
+
+router.post("/signin", (req, res) => {
+    /* handle signin */
+  });
+
+ 
+module.exports = router;
