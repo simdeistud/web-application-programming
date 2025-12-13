e539bb87bec34340f576c408d0d84cda2e473b3e
simdeistud <simone.deiana@studenti.units.it>
2025-12-13 18:59:18 +0100

    Added support for base64 stuff

--- EXAM CALCETTO PROJECT/src/server/api.js
+++ EXAM CALCETTO PROJECT/src/server/api.js
@@ -9,11 +9,16 @@ router.use("/auth", auth);
 router.use("/fields", fields);
 router.use("/tournaments", tournaments);
 router.use("/matches", matches);
-	
+
 // GET  /api/whoami  If authenticated, returns information about the current user
-router.get("/whoami", (req, res, next) => { 
-    res.writeHead(401);
+router.get("/whoami", (req, res, next) => {
+  if ("user is authenticated") {
+    res.header(200).json({ user });
     res.end();
-  });
- 
+    return;
+  }
+  res.header(401).json({ error: "Not authenticated" });
+  res.end();
+});
+
 module.exports = router;
