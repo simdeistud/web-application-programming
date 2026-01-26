a5654d63e753d4c06841a15f38c905ae13850c77
simdeistud <simone.deiana@studenti.units.it>
2026-01-26 20:55:58 +0100

    Added initial login and registration forms

--- EXAM CALCETTO PROJECT/web/js/client.js
+++ EXAM CALCETTO PROJECT/web/js/client.js
@@ -3,29 +3,40 @@ const registerButton = document.getElementById('btn-register');
 
 if (loginButton && registerButton) {
     loginButton.addEventListener('click', () => {
-        const frame = document.querySelector('.frame');
+        const frame = document.querySelector('main');
         // Inject login form
         frame.innerHTML = `
-    <div class="login-wrapper">
-      <h2>Login</h2>
-      <form id="login-form" autocomplete="on" novalidate>
-        <div class="field">
-          <label for="username">Username</label>
-          <input id="username" name="username" type="text" required />
-        </div>
-        <div class="field">
-          <label for="password">Password</label>
-          <input id="password" name="password" type="password" required />
-        </div>
-        <button id="login-submit" type="submit">Login</button>
-      </form>
-      <p id="login-error" role="alert" style="color:#c00;display:none;"></p>
-    </div>
-  `;
-
+        <form id="form-login">
+            <fieldset>
+                <legend>[LOGIN]</legend>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required />
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+                <button id="btn-login-form">[SUBMIT]</button>
+            </fieldset>
+        </form>
+        `;
     });
 
     registerButton.addEventListener('click', () => {
-        alert('Register button clicked');
+        const frame = document.querySelector('main');
+        // Inject registration form
+        frame.innerHTML = `
+        <form id="form-register">
+            <fieldset>
+                <legend>[REGISTER]</legend>
+                <label for="username">Username:</label>
+                <input type="text" id="username" name="username" required />
+                <label for="first-name">First Name:</label>
+                <input type="text" id="first-name" name="first-name" required />
+                <label for="last-name">Last Name:</label>
+                <input type="text" id="last-name" name="last-name" required />
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+                <button id="btn-register-form">[SUBMIT]</button>
+            </fieldset>
+        </form>
+        `;
     });
 }
